fix: update hmc code so that jacobian works on numpy array

src/hmc_optim.py
@@ -4,41 +4,26 @@ import os
 from datetime import datetime
 from functools import partial
 
-import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
 import mndo
 from data import load_data, prepare_data
-from objective import jacobian_parallel, penalty
-from hmc_utils import (
-    sample_chain,
-    trace_fn_nuts,
-    get_nuts_kernel,
-    trace_fn_hmc,
-    get_hmc_kernel,
-)
+from objective import jacobian, jacobian_parallel, penalty
+from hmc_utils import sample_chain, trace_fn_nuts, get_nuts_kernel
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 # %%
 # Load the reference data
 mols_atoms, mols_coords, charges, titles, reference = load_data(query_size=100)
-ref_energies = reference.iloc[:, 1].tolist()
+ref_energies = reference["binding_energy"].tolist()
 
 # NOTE we should refactor so that we don't need _tmp_molecules here
-tmp_molecule_file = "_tmp_molecules"
+filename = "_tmp_molecules"
 mndo_method = "MNDO"
 mndo.write_tmp_optimizer(
-    atoms=mols_atoms, coords=mols_coords, filename=tmp_molecule_file, method=mndo_method
-)
-
-mndo_input = mndo.get_inputs(
-    atoms_list=mols_atoms,
-    coords_list=mols_coords,
-    charges=np.zeros_like(mols_atoms),
-    titles=range(len(mols_atoms)),
-    method=mndo_method,
+    atoms=mols_atoms, coords=mols_coords, filename=filename, method=mndo_method
 )
 
 # %%
@@ -59,32 +44,25 @@ param_keys, _ = prepare_data(mols_atoms, mean_params)
 # param_values = [tf.constant(x) for x in param_values]
 param_values = [tf.random.truncated_normal([], stddev=1.0) for _ in param_keys]
 
+kwargs = {
+    "param_keys": param_keys,
+    "filename": filename,
+    "ref_props": ref_energies,
+    "mean_params": mean_params,
+    "scale_params": scale_params,
+    "n_procs": 2,
+    "binary": "/home/reag2/PhD/second-year/bayes-mndo/mndo/mndo99_binary",
+}
+
 # %%
 @tf.custom_gradient
 def target_log_prob_fn(*param_vals):
-    log_likelihood = -penalty(
-        param_vals,
-        param_keys,
-        filename=tmp_molecule_file,
-        ref_props=ref_energies,
-        mean_params=mean_params,
-        scale_params=scale_params,
-        binary="/home/reag2/PhD/second-year/bayes-mndo/mndo/mndo99_binary",
-    )
+    log_likelihood = -penalty(param_vals, **kwargs)
 
     def grad_fn(*dys):
-        grad = jacobian_parallel(
-            param_vals,
-            mndo_input=mndo_input,
-            param_keys=param_keys,
-            dh=1e-5,
-            # filename=tmp_molecule_file,
-            ref_props=ref_energies,
-            mean_params=mean_params,
-            scale_params=scale_params,
-            binary="/home/reag2/PhD/second-year/bayes-mndo/mndo/mndo99_binary",
-        )
-        return grad
+        # grad = jacobian(param_vals, dh=1e-5, **kwargs)
+        grad = jacobian_parallel(param_vals, dh=1e-5, **kwargs)
+        return grad.tolist()
 
     return log_likelihood, grad_fn
 
@@ -120,24 +98,5 @@ chain, trace, final_kernel_results = sample_chain(
     trace_fn=partial(trace_fn_nuts, summary_writer=summary_writer),
 )
 
-# chain, trace, final_kernel_results = sample_chain(
-#     num_results=30,
-#     current_state=param_values,
-#     kernel=get_hmc_kernel(real_target_log_prob_fn, step_size, n_adapt_steps),
-#     return_final_kernel_results=True,
-#     trace_fn=partial(trace_fn_hmc, summary_writer=summary_writer),
-# )
-
 with open("../parameters/parameters-opt-hmc.json", "w") as f:
     json.dump([list(x) for x in chain], f)
-
-# # %%
-# fig, axs = plt.subplots(2, 2)
-# axs[0, 0].hist(chain)
-# axs[0, 0].set_title("chain histogram")
-# axs[0, 1].plot(chain)
-# axs[0, 1].set_title("chain plot")
-# axs[1, 0].hist(trace)
-# axs[1, 0].set_title("trace histogram")
-# axs[1, 1].plot(trace)
-# axs[1, 1].set_title("trace plot")

src/hmc_test.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 
 from hmc_utils import (
     sample_chain,
-    trace_fn,
+    trace_fn_nuts,
     get_nuts_kernel,
 )
 from bo_bench import (
@@ -40,8 +40,8 @@ def jacobian(*params, dh=1e-5):
         params: values for each Branin-Hoo param
         dh: small value for numerical gradients
     """
-    grad = [0] * len(params)
     params = list(params)
+    grad = np.zeros_like(params)
 
     for i, _ in enumerate(params):
         params[i] += dh
@@ -64,7 +64,7 @@ def custom_grad_target_log_prob_fn(*params):
 
     def grad_fn(*dys):
         grad = jacobian(*params)
-        return grad
+        return grad.tolist()
 
     return log_likelihood, grad_fn
 
@@ -104,24 +104,31 @@ chain, trace, final_kernel_results = sample_chain(
     current_state=init_state,
     kernel=get_nuts_kernel(target_log_prob_fn, step_size, n_adapt_steps),
     return_final_kernel_results=True,
-    trace_fn=partial(trace_fn, summary_writer=summary_writer),
+    trace_fn=partial(trace_fn_nuts, summary_writer=summary_writer),
 )
 burnin, samples = chain[:n_adapt_steps], chain[n_adapt_steps:]
 
 
 # %%
-# plot_funcs = [
-#     [branin_hoo_fn, "Electric"],
-#     [branin_hoo_factory(*init_state), "Viridis"],  # default colorscale
-#     [branin_hoo_factory(*chain[-1].numpy()), "Blues"],
-# ]
-# surfaces = [
-#     go.Surface(
-#         x=xr, y=yr, z=fn(domain).reshape(len(yr), -1), colorscale=cs, showscale=False
-#     )
-#     for fn, cs in plot_funcs
-# ]
-# samples_plot = go.Scatter3d(x=xy[0], y=xy[1], z=z_true, mode="markers")
-# fig = go.Figure(data=[*surfaces, samples_plot])
-# title = "Branin-Hoo (bottom), initial surface (top), HMC final surface (middle)"
-# fig.update_layout(height=700, title_text=title)
+import plotly.graph_objects as go
+
+# Plot the Branin-Hoo surface
+xr = np.linspace(-5, 15, 21)
+yr = np.linspace(0, 10, 11)
+domain = np.stack(np.meshgrid(xr, yr), -1).reshape(-1, 2).T
+
+plot_funcs = [
+    [branin_hoo_fn, "Electric"],
+    [branin_hoo_factory(*init_state), "Viridis"],  # default colorscale
+    [branin_hoo_factory(*chain[-1].numpy()), "Blues"],
+]
+surfaces = [
+    go.Surface(
+        x=xr, y=yr, z=fn(domain).reshape(len(yr), -1), colorscale=cs, showscale=False
+    )
+    for fn, cs in plot_funcs
+]
+samples_plot = go.Scatter3d(x=xy[0], y=xy[1], z=z_true, mode="markers")
+fig = go.Figure(data=[*surfaces, samples_plot])
+title = "Branin-Hoo (bottom), initial surface (top), HMC final surface (middle)"
+fig.update_layout(height=700, title_text=title)